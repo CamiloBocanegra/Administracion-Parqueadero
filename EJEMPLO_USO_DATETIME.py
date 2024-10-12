c23178037854fe82e62f75bcb16949ba40eff60a
Camilo Bocanegra <camilob.villa@gmail.com>
2024-10-12 15:24:04 -0500

    ejemplo uso datetime y estructura de administracion_parqueadero.py terminada y comentada

---
+++ EJEMPLO_USO_DATETIME.py
@@ -0,0 +1,28 @@
+
+from datetime import datetime
+
+
+time1 = datetime.now()
+time1 = datetime.now()
+
+
+while(True):
+   print("1. registrar entrada \n aaaaa")
+   print("2. registrar salida")
+   print("3. calcular tiempo transcurrido")
+   
+   opcion_seleccionada = int(input("Digite una opcion: "))
+
+   if(opcion_seleccionada == 1):      
+      time1 = datetime.now()
+      print(time1)
+   elif(opcion_seleccionada == 2):
+      time2 = datetime.now()
+      print(time2)
+   elif(opcion_seleccionada == 3):
+      difference = time2-time1
+      print(difference)
+      print(f"dias transcurridos: {difference.days}")
+      print(f"segundos transcurridos: {difference.seconds}")
+      print(f"horas transcurridas {difference.seconds / 3600}")
+      break
