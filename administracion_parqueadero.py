7b1cb883e7771c0563b5b942c83dd92c9958a2f9
Camilo Bocanegra <camilob.villa@gmail.com>
2024-10-16 22:05:22 -0500

    cambiando lista de diccionarios a lista de cadenas(estado)

--- administracion_parqueadero.py
+++ administracion_parqueadero.py
@@ -1,5 +1,5 @@
 from matriz import mostrar_matrices
-# from Alquiler import alquiler
+from Alquiler import alquiler
 # from registrar_salida import registrar_salida
 # from registrar_entrada import registrar_entrada
 # from Facturar import Facturar
@@ -9,8 +9,8 @@ from datetime import datetime
 
 # van a ser listas/matrices de diccionarios
 # estado O:ocupado, A:alquilado, D:disponible
-lista_carros = [{"numero":i, "estado":"D"} for i in range(50)]
-lista_motos = [{"numero":i, "estado":"D"} for i in range(25)]
+lista_carros = ["D" for i in range(50)]
+lista_motos = ["D" for i in range(25)]
 
 # ejemplos para visualizar la matriz en caso de que estos espacios estuvieran ocupados
 lista_carros[2]["estado"] = "A"
@@ -21,7 +21,7 @@ lista_motos[15]["estado"] = "O"
 
 while(True):
    print("1. Mostrar matriz del parqueadero")
-#    print("2. Alquiler (entrada del vehículo por placa y tipo (vehículo o moto).")
+   print("2. Alquiler (entrada del vehículo por placa y tipo (vehículo o moto).")
 #    print("3. Registrar (hora entrada del vehículo por placa y tipo (vehículo o moto).")
 #    print("4. Registrar (hora salida del vehículo por placa y tipo (vehículo o moto).")
 #    print("5. Facturar")
@@ -32,8 +32,8 @@ while(True):
 
    if(opcion_seleccionada == 1):      
       mostrar_matrices(lista_carros, lista_motos)
-#    elif(opcion_seleccionada == 2):
-#       alquiler(lista_carros, lista_motos)
+   elif(opcion_seleccionada == 2):
+      alquiler(lista_carros, lista_motos)
 #    elif(opcion_seleccionada == 3):
 #       registrar_entrada(lista_carros, lista_motos)
 #    elif(opcion_seleccionada == 4):
