f8d6902f684162849c0b202660116b341afc53e5
Erika Rodriguez Cañar <erikarodriguezc123@gmail.com>
2024-10-16 21:14:02 -0500

    actualizar alquiler

--- Alquiler.py
+++ Alquiler.py
@@ -1,7 +1,17 @@
-def alquiler(parqueadero, tipo_vehiculo, placa):
-      for fila in parqueadero:
+from matriz import mostrar_matrices
+
+def alquiler(matriz):
+      tipo_vehiculo = input("Tipo de vehiculo:")
+      placa = input("Ingrese número de placa: ")
+      for fila in matriz:
         for i, espacio in enumerate(fila):
             if espacio[0] == tipo_vehiculo and espacio == 'A':
                 fila[i] = f"{tipo_vehiculo}{placa}"
                 return True
         return False
+       
+      if alquiler (matriz):
+          print ("Espacio asignado")
+      else:
+          print ("No hay espacios disponibles")
+    
