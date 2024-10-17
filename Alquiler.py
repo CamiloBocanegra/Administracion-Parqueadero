e017bce44e8263d4473930932bf9503c22d0c3b5
Erika Rodriguez Cañar <erikarodriguezc123@gmail.com>
2024-10-16 22:40:30 -0500

    Actualizacion

--- Alquiler.py
+++ Alquiler.py
@@ -1,17 +1,23 @@
 from matriz import mostrar_matrices
 
-def alquiler(matriz):
-      tipo_vehiculo = input("Tipo de vehiculo:")
-      placa = input("Ingrese número de placa: ")
-      for fila in matriz:
-        for i, espacio in enumerate(fila):
-            if espacio[0] == tipo_vehiculo and espacio == 'A':
-                fila[i] = f"{tipo_vehiculo}{placa}"
-                return True
-        return False
-       
-      if alquiler (matriz):
-          print ("Espacio asignado")
-      else:
-          print ("No hay espacios disponibles")
+def alquiler(lista_carros, lista_motos):
+    tipo_vehiculo = input("Tipo de vehiculo:")
+    placa = input("Ingrese número de placa: ")
+    numero_espacio = int(input ("Ingrese número espacio que se va a alquilar:"))-1
+
+    lista_vehiculos = lista_motos
+
+    if tipo_vehiculo == "Carro":
+        lista_vehiculos = lista_carros
+    elif tipo_vehiculo == "moto":
+        lista_vehiculos = lista_motos
     
+    if lista_vehiculos[numero_espacio] != "D":
+            print("Espacio no esta disponible")
+    else:
+        lista_vehiculos[numero_espacio] = "A"
+        print(f"El espacio {numero_espacio} ha sido alquilado")
+
+     
+    
+ 
